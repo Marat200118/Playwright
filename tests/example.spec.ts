c4b90534ac2cbcccb1bd0e05d935627d4132b6df
Marat200118 <marat.samigullin1@gmail.com>
2024-10-01 17:32:08 +0200

    tried to connect the lighthouse with playwright

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+import { playAudit } from 'playwright-lighthouse';
+
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -30,20 +32,37 @@ test.describe('navigation', () => {
   });
 });
 
-// test('Test website on iPhone 12', async ({ browser }) => {
-//   const iPhone = browser.newContext({
-//     viewport: { width: 390, height: 844 },
-//     userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A372 Safari/604.1',
-//     deviceScaleFactor: 3,
-//     isMobile: true,
-//     hasTouch: true,
-//   });
+test('Test Portfolio website on iPhone 12', async ({ browser }) => {
 
-//   const page = await iPhone.newPage();
-//   await page.goto('https://www.example.com');
-//   await expect(page).toHaveTitle(/Example/);
-//   // Add more checks based on the mobile experience
-// });
+  const iPhone = await browser.newContext({
+    viewport: { width: 390, height: 844 },
+    userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A372 Safari/604.1',
+    deviceScaleFactor: 3,
+    isMobile: true,
+    hasTouch: true,
+  });
+
+  const page = await iPhone.newPage();
+  
+  await page.goto('https://www.marats-samigullins.com/portfolio');
+  await page.screenshot({ path: 'screenshots/iphone-12.png' });
+
+  await page.getByRole('heading', { name: 'About' }).click();
+  await page.screenshot({ path: 'screenshots/iphone-12-about.png' });
+
+  await page.getByRole('heading', { name: 'Skills' }).click();
+  await page.screenshot({ path: 'screenshots/iphone-12-skills.png' });
+
+  await page.getByRole('heading', { name: 'Education', exact:true }).click();
+  await page.screenshot({ path: 'screenshots/iphone-12-education.png' });
+
+  await page.getByRole('heading', { name: 'Projects', exact:true }).click();
+  await page.screenshot({ path: 'screenshots/iphone-12-projects.png' });
+
+  await page.getByRole('heading', { name: 'Contact', exact:true }).click();
+  await page.screenshot({ path: 'screenshots/iphone-12-contact.png' });
+  
+});
 
 
 
@@ -64,3 +83,20 @@ test.describe('Responsive Design Test', () => {
     });
   });
 });
+
+
+// test('Run Lighthouse audit on website', async ({ page }) => {
+//   await page.goto('https://www.example.com');
+//   const report = await playAudit({
+//     page: page,
+//     thresholds: {
+//       performance: 50,
+//       accessibility: 50,
+//       'best-practices': 50,
+//       seo: 50,
+//       pwa: 50,
+//     },
+//     port: 9222,
+//   });
+//   console.log(report);
+// });
